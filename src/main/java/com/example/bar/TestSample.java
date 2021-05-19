c1901cc6a2e7c3fe1fbf03775686d8263ddc7413
vagusX <vagusxl@gmail.com>
2021-05-19 11:15:56 +0800

    chore: update

---
+++ src/main/java/com/example/bar/TestSample.java
@@ -0,0 +1,7 @@
+package com.example.bar;
+
+public class TestSample {
+  public static String getUserId() {
+    return "123";
+  }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.foo.DataExample;
+import com.example.bar.TestSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		DataExample.getUserId();
+		TestSample.getUserId();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
